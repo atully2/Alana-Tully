ebebb5fc8deded0635f91f09672ae16e310d6340
atully2 <107014964+atully2@users.noreply.github.com>
2022-07-13 22:39:56 -0400

    Start

--- main.js
+++ main.js
@@ -1,60 +1,9 @@
-<!DOCTYPE html>
+$(document).ready(function() {
 
-<html>
- 
-  <head>
-    <title>HTML Lesson</title>
-    <meta name="keywords" content="lesson, HTML, Binghamton University">
-    <meta name="description" content="This is a basic intro to HTML coding">
-    <link rel="stylesheet" type="text/css" href="css/untitled">
-  </head//>
-  <body>
+var $hamburger = $(".hamburger");
+  $hamburger.on("click", function(e) {
+    $hamburger.toggleClass("is-active");
+    $('#nav').toggleClass('active');
+  });
 
-    <h1 id="main-headline">Intro to HTML lesson</h1>
-    <h2>My Very nice subtitle</h2>
-    <h3>Another Headline Right Here</h3>
-    <h4>This is a even smaller header</h4>
-
-    <!-- This is a comment -->
-
-    <p>
-      This group’s task was a mix of on-the-fly science, <em>New York Times</em> public communication and brand management: Blow everyone’s mind, <strong>hi</strong>show policymakers what all those appropriations had paid for, and assure the <i>hi</i>rest of the scientific world that yes, some of the universe’s most elusive secrets might at long last be within reach.
-    </p>
-
-      <img src="downloads/puppies2.jpg" title="My Doggies" alt="Here are my pups" width="500" />
-
-    <blockquote>
-      <p>
-        Learn to conceive, design, and code websites using the Document Object Model and
-      </p>
-    </blockquote>
-
-    <!-- Another Comment -->
-
-    <p>
-      This group’s task was a mix of on-the-fly science, public communication and brand management: Blow everyone’s mind, show policymakers what all <a target="_blank" title="Link to Google" href="https://google.com">Google</a> had paid for, and assure the rest of the scientific world that yes, some of the universe’s most elusive secrets might at long last be within reach.
-    </p>
-
-    <p>The Catcher in the Rye<br>
-      Ulysses<br>
-      The Plague
-    </p>
-
-    <ul>
-      <li>apples</li>
-      <li>oranges</li>
-      <li>pears</li>
-      <li>blueberries</li>
-    </ul>
-
-    <ol>
-      <li>New York</li>
-      <li>New Jersey</li>
-      <li>Connecticut</li>
-    </ol>
-    
-  </body>
-
-
-
-</html> 
+});
